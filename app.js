16232bafe629b61fcd82d78db985a5beac4695e0
Jia Rong Tsai <jiarongtsai19@gmail.com>
2021-12-24 14:06:02 +0800

    feat: install the handlebars view engine

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 // require modules
 const express = require('express')
+const exphbs = require('express-handlebars')
+
 const app = express()
 
+// set the handlebars as view engine
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname:'hbs'}))
+app.set('view engine', 'hbs')
+
 app.get('/', (req, res)=>{
-    res.send('Hello World')
+    res.render('index')
 })
 
 app.listen(3000, (req, res)=>{
