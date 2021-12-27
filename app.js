4ddfcf2d0b59a9257a8e702a9349c4b4c3180c26
Jia Rong Tsai <jiarongtsai19@gmail.com>
2021-12-27 16:53:00 +0800

    style: standard JS

--- app.js
+++ app.js
@@ -5,29 +5,29 @@ const exphbs = require('express-handlebars')
 const app = express()
 
 // set the handlebars as view engine
-app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname:'hbs'}))
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
 
-//styleing
+// styleing
 app.use(express.static('public'))
 
-//route
-app.get('/', (req, res)=>{
-    res.render('index')
+// route
+app.get('/', (req, res) => {
+  res.render('index')
 })
 
-app.get('/about', (req, res)=>{
-    res.render('about')
+app.get('/about', (req, res) => {
+  res.render('about')
 })
 
-app.get('/portfolio', (req, res)=>{
-    res.render('portfolio')
+app.get('/portfolio', (req, res) => {
+  res.render('portfolio')
 })
 
-app.get('/contact', (req, res)=>{
-    res.render('contact')
+app.get('/contact', (req, res) => {
+  res.render('contact')
 })
 
-app.listen(3000, (req, res)=>{
-    console.log('The server is listening on port 3000.')
+app.listen(3000, (req, res) => {
+  console.log('The server is listening on port 3000.')
 })
